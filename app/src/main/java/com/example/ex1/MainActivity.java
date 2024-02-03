a001473f58c5a6b2b75de196de040b3cf2d497ed
lucasvec <dosgamesxxx@gmail.com>
2024-02-03 15:51:36 -0300

    Inserção da imagem Android vinculando com ação onClick com reação do toast, alteração do icone do app

--- app/src/main/java/com/example/ex1/MainActivity.java
+++ app/src/main/java/com/example/ex1/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+ImageView imageAndroid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageAndroid = findViewById(R.id.imageAndroid);
+
+        imageAndroid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Olá!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
+
+
+
 }
